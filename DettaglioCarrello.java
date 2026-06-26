ac92e8603fb42e49f8e3fe9362ffb52b53ab6043
aiovino36-lang <a.iovino36@studenti.unisa.it>
2026-06-26 14:06:10 +0200

    Add files via upload

---
+++ DettaglioCarrello.java
@@ -0,0 +1,21 @@
+package model;
+
+public class DettaglioCarrello {
+
+private int idCarrello;
+private int idProdotto;
+private int quantita;
+
+public DettaglioCarrello(){}
+
+public int getIdCarrello() {return idCarrello;}
+public void setIdCarrello(int idCarrello) {this.idCarrello=idCarrello;}
+
+public int getIdProdotto() {return idProdotto;}
+public void setIdProdotto(int idProdotto) {this.idProdotto=idProdotto;}
+
+public int getQuantita() {return quantita;}
+public void setQuantita(int quantita) {this.quantita=quantita;}
+	
+	
+}
---
+++ Utente.java
@@ -0,0 +1,51 @@
+package model;
+
+import java.sql.Date;
+
+public class Utente {
+    private int id;
+    private int tipoUtente;      // 1=admin, 2=staff, 3=cliente [cite: 5, 25]
+    private String codiceUtente;
+    private int statoAccount;    // 1=attivo, 2=sospeso, 3=in attesa
+    private String nome;
+    private String cognome;
+    private Date dataNascita;
+    private String numeroTelefono;
+    private String email;
+    private String passwordU;
+
+    // Costruttore vuoto obbligatorio per i Java Bean [cite: 68]
+    public Utente() {
+    }
+
+    // Metodi Getter e Setter [cite: 68]
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
+    public int getTipoUtente() { return tipoUtente; }
+    public void setTipoUtente(int tipoUtente) { this.tipoUtente = tipoUtente; }
+
+    public String getCodiceUtente() { return codiceUtente; }
+    public void setCodiceUtente(String codiceUtente) { this.codiceUtente = codiceUtente; }
+
+    public int getStatoAccount() { return statoAccount; }
+    public void setStatoAccount(int statoAccount) { this.statoAccount = statoAccount; }
+
+    public String getNome() { return nome; }
+    public void setNome(String nome) { this.nome = nome; }
+
+    public String getCognome() { return cognome; }
+    public void setCognome(String cognome) { this.cognome = cognome; }
+
+    public Date getDataNascita() { return dataNascita; }
+    public void setDataNascita(Date dataNascita) { this.dataNascita = dataNascita; }
+
+    public String getNumeroTelefono() { return numeroTelefono; }
+    public void setNumeroTelefono(String numeroTelefono) { this.numeroTelefono = numeroTelefono; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+
+    public String getPasswordU() { return passwordU; }
+    public void setPasswordU(String passwordU) { this.passwordU = passwordU; }
+}
